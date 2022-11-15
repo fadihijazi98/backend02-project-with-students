3d2c6c93b871c6810cbc2a54b13b1159ef828be2
fadi.hijazi <fadihijazi2018@outlook.com>
2022-11-15 18:10:16 +0200

    update PHPDoc string.

--- index.php
+++ index.php
@@ -4,7 +4,9 @@ require 'vendor/autoload.php';
 /*
  * use Dotenv &
  * load environment variables.
+ * use `$_ENV` to access variables.
  * (safeLoad) to skip exceptions if `.env` not exist
  */
 use Dotenv\Dotenv;
 Dotenv::createImmutable(__DIR__)->safeLoad();
+
