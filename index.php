76933033ec140bf7116dae764fdf1ba16aeec46e
donia hasan <doniaziyad@hotmail.com>
2023-06-25 23:59:20 +0300

    setup dependency and create first model called : User

--- index.php
+++ index.php
@@ -3,15 +3,34 @@
 require 'vendor/autoload.php';
 header('Content-Type: application/json; charset=utf-8');
 
+use Illuminate\Database\Capsule\Manager as Capsule;
 use Components\Route;
+
 require 'routes/v1/route.php';
 require 'routes/v2/route.php';
+
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver' => 'mysql',
+    'host' => '127.0.0.1',
+    'database' => 'backend02-project-with-students',
+    'username' => 'root',
+    'password' => '',
+]);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$users =\Model\User::all();
+var_dump($users);die();
 
 try {
 
     $response = Route::handleRequest();
 
-} catch (Exception $e){
+} catch (Exception $e) {
     $response = [
         'message' => $e->getMessage()
     ];
