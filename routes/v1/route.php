d90d99ffab02ef5351702bce512519c2197f6963
fadi.hijazi <fadihijazi2018@outlook.com>
2023-07-01 10:15:41 +0300

    cleanup.

--- routes/v1/route.php
+++ routes/v1/route.php
@@ -1,17 +1,1 @@
 <?php
-use Controllers\UserController;
-use Components\Route;
-
-Route::setVersion(1);
-
-// GET /users
-Route::GET("users", UserController::class);
-// GET /users/{id}
-Route::GET("users/{id}", UserController::class, "show");
-// POST /users
-Route::POST("users", UserController::class);
-// PUT /users/{id}
-Route::PUT("users/{id}", UserController::class);
-// DELETE /users/{id}
-Route::DELETE("users/{id}", UserController::class);
-
