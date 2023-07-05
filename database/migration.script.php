1f9f849b36b834ee20fef0c80d9ceeef03b81d8e
donia hasan <doniaziyad@hotmail.com>
2023-07-05 14:13:02 +0300

    cleanUp

--- database/migration.script.php
+++ database/migration.script.php
@@ -1,6 +1,7 @@
 <?php
 require 'bootstrap.php';
 use Illuminate\Database\Capsule\Manager;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 $migrations = glob(__DIR__ . "/migrations/*");
 
@@ -12,6 +13,7 @@ foreach ($migrations as $migration){
     $script_name=array_pop($_);
 
     $table_name = explode(".",$script_name)[1];
+    var_dump(\Models\User::query()->findOrFail("3"));die();
 
     if (Manager::schema()->hasTable($table_name)){
         echo "$table_name table already exists !\n";
