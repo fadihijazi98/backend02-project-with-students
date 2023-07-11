2446d60ceb5823cd03c19fd3729a3e94b5d0dbe9
Saleh Zetawi <salehzetawi15@gmail.com>
2023-07-12 01:39:32 +0300

    setup dependency & create first model called 'User'

--- index.php
+++ index.php
@@ -1,14 +1,42 @@
 <?php
 
+header('Content-Type:application/json; charset=utf-8');
 
 require 'vendor/autoload.php';
-header('Content-Type:application/json; charset=utf-8');
-use Controller\CommentsController;
-use Controller\LikesController;
-use component\Route;
+
+/*
+ * require routes for several versions
+ * */
+
 require "routes/v1/route.php";
 /*require "routes/v2/route.php";*/
 
+
+use Controller\CommentsController;
+use Controller\LikesController;
+use component\Route;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+/**
+ * setup database dependencies
+ * */
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver' => 'mysql',
+    'host' => '127.0.0.1',
+    'database' => 'Facebook-API',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+]);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+/*$users=\Models\User::all();*/
 
 try {
 $response=Route::handelRequest();
